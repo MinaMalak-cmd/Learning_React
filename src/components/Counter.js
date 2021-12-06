7b19aee53d5d8b38bdb979a5f173e6d3b47ddc12
MinaMalak-cmd <engmina15@yahoo.com>
2021-12-06 19:29:49 +0200

    add provider & selector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,17 @@
+import {useSelector, useDispatch} from "react-redux";
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
-
+  const toggleCounterHandler = () => {
+     // useDispatch({type:"increment"})
+     
+  };
+  const getCertainData = state => state.counter+1;
+  const counter = useSelector(getCertainData);
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,18 @@
-const store = require("redux");
-store.createStore(reducerFn);
-const reducerFn = (state,action) =>{
-    return {
-        state:state,
-        type:action.type,
-    }
+import {createStore} from "redux"
+
+const reducerFn = (state={counter:1},action) =>{
+    switch(action.type){
+        case "increment":
+           return {
+              counter : state.counter+1
+           };
+           case "decrement":
+           return {
+              counter : state.counter-1
+           };
+           default :
+           return state;
+       }  
 }
+const store = createStore(reducerFn);
+export default store;
