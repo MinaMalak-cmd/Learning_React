5f277a6a69b47245c85b2eecb00413be0ba58070
MinaMalak-cmd <engmina15@yahoo.com>
2021-12-07 19:21:23 +0200

    add dummy text in BlogDetail

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -2,7 +2,10 @@ import { useParams } from "react-router-dom";
 const BlogDetail = () => {
     const {id} = useParams();
     return ( 
-        <div>{id}</div>
+        <div>{id}
+        trial
+        </div>
+
      );
 }
 export default BlogDetail;
