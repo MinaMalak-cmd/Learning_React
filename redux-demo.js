4c124e639f2bc1f710052d38db9074e830042db7
Mina Malak <mina.malak@tactful.ai>
2022-09-23 23:49:56 +0200

    improve code

--- redux-demo.js
+++ redux-demo.js
@@ -1,26 +1,26 @@
-const redux = require('redux');
+const redux = require("redux");
 
-const counterReducer = (state={counter:0},action)=>{
- switch(action.type){
-	 case "increment":
-		return {
-		   counter : state.counter+1
-		};
-        case "decrement":
-		return {
-		   counter : state.counter-1
-		};
-        default :
-		return state;
-	}  
- 
- };
-const store = redux.createStore(counterReducer);
-const counterSubscriber = () =>{
+const counterReducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+};
+const store = redux.createStore(counterReducer); //takes a reducer function
+const counterSubscriber = () => {
   const latestState = store.getState();
-  console.log(latestState);    
-}
+  console.log(latestState);
+};
 
-store.subscribe(counterSubscriber);
-store.dispatch({type:"increment"});
-store.dispatch({type:"decrement"});
+store.subscribe(counterSubscriber); //takes a subscriber function
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
+//to run the file write node redux-demo.js
